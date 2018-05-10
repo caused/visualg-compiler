3c3c0c1373ddf69b1ce68f4b9c2a93e468b2aa71
GUSTAVO ALVES DOS SANTOS SILVA <galves@totvs.com.br>
2018-05-10 16:24:07 -0300

    Termino da analise sintatica

--- projeto.c
+++ projeto.c
@@ -1,7 +1,6 @@
 /*
 	Gustavo Alves dos Santos Silva - 31504116
 	Cassiano Moura Leão - 31574564
-	Veridiana Lopes - 31510671
 
 */
 
@@ -1038,7 +1037,7 @@ char *leitorLexico(char str[],int *p) {
 		i++;
 		if(str[i] == '\0'){
 			*p = i;
-			return "-";
+			return "opsub";
 		} else{
 			return "erro";
 		}
@@ -1143,6 +1142,7 @@ struct Node{
 	int escopo;
     char token[100];
     char lexema[100];
+    int linha;
  struct Node *prox;
 };
 
@@ -1153,8 +1153,8 @@ int tam;
 
 void inicia(node *FILA);
 int vazia(node *FILA);
-node *aloca(char* elem,char* s);
-void enqueue(node *FILA, char* elem,char* s);
+node *aloca(char* elem,char* s, int l);
+void enqueue(node *FILA, char* elem,char* s, int l);
 void exibe(node *FILA); // Metodo usado para testes
 void libera(node *FILA);
 
@@ -1171,7 +1171,7 @@ int vazia(node *FILA){
 }
 
 //aloca o token lido no analisador léxico
-node *aloca(char* elem,char* s){ 
+node *aloca(char* elem,char* s, int l){ 
 	node *novo=(node *) malloc(sizeof(node));
 	if(!novo){
 		printf("Sem memoria disponivel!\n");
@@ -1179,6 +1179,7 @@ node *aloca(char* elem,char* s){
 	}else{
        strcpy (novo->token, elem);
 	   strcpy (novo->lexema, s);
+	   novo->linha = l;
 	   return novo;
 	}
 }
@@ -1211,8 +1212,8 @@ void libera(node *FILA){
 	}
 }
 
-void enqueue(node *FILA, char* elem,char* s){
-    node *novo=aloca(elem,s);
+void enqueue(node *FILA, char* elem,char* s, int l){
+    node *novo=aloca(elem,s,l);
    	novo->prox = NULL;
 	if(vazia(FILA))
 		FILA->prox=novo;
@@ -1224,7 +1225,6 @@ void enqueue(node *FILA, char* elem,char* s){
 	}
 	tam++;
 }
-
 int buscaSimbolo(node *token, char *elemento){
  	int i = 1;
     if(token->prox == NULL){
@@ -1251,6 +1251,539 @@ int buscaSimbolo(node *token, char *elemento){
     }
 }
 
+//Analise sintática
+
+//retira o elemento a partir do topo da lista
+node *dequeue(node *FILA){ 
+	if(FILA->prox == NULL){
+		printf("Fila ja esta vazia\n");
+		return NULL;
+	}else{
+		node *tmp = FILA->prox;
+		FILA->prox = tmp->prox;
+		tam--;
+		return tmp;
+	}
+}
+
+//Retorna primeiro elemento da lista de tokens
+node *head(node *FILA){
+    if(FILA->prox == NULL){
+       exit(0);
+    }else{
+        node *tmp = FILA->prox;
+		return tmp;
+    }
+}
+
+char *lookahead(node *FILA){
+    if(FILA->prox == NULL){
+       exit(0);
+    }else{
+        node *tmp = FILA->prox;
+		return tmp->token;
+    }
+}
+
+
+void match(node *FILA, char *token){
+	if(strcmp(lookahead(FILA), token) == 0){	
+		dequeue(FILA);
+	}else{
+		printf("ERRO SINTATICO\n");
+		exit(1);
+	}	
+}
+
+
+void ID(node *BUFFER, int escopo){
+    node *token;
+	token = head(BUFFER);
+	char *retorno;
+	retorno = lookahead(BUFFER);
+	
+	if(strcmp(retorno, "id") == 0){	
+		match(BUFFER, "id");		
+	}else{
+		printf("Era esperado <id>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+		exit(1);
+	}
+}
+
+void ListaID(node *BUFFER, int escopo){
+	ID(BUFFER, escopo);	
+    if (strcmp(lookahead(BUFFER), "opvirg") == 0) {
+    	while(strcmp(lookahead(BUFFER), "opvirg") == 0) {
+    		match(BUFFER, "opvirg");
+			ID(BUFFER, escopo);
+		}
+    }
+}
+
+
+void TIPO(node *BUFFER,  int escopo){ 
+    node *token;
+	token = head(BUFFER);
+	
+	if(strcmp(lookahead(BUFFER), "inteiro") == 0){
+		match(BUFFER, "inteiro");
+	}else if(strcmp(lookahead(BUFFER), "logico") == 0){
+		match(BUFFER, "logico");
+	}else{
+		 printf("Era esperado <inteiro> ou <logico>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+		 exit(1);
+	}
+	
+}
+
+
+//DeclVar -> ListaID : TIPO
+void DeclVar(node *BUFFER, int escopo){
+	node *token;
+	ListaID(BUFFER, escopo); 
+	token = head(BUFFER);
+	if(strcmp(lookahead(BUFFER), "opreldoispont") == 0){
+	    match(BUFFER,"opreldoispont");
+		TIPO(BUFFER, escopo);		
+	}else{ 
+		printf("Era esperado <opreldoispont>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+		exit(1);	
+	}
+}
+
+//PartDeclVar -> var DeclVar { DeclVar}  
+void PartDeclVar(node *BUFFER, int escopo, int qtde){
+	node *token;
+	token = head(BUFFER);
+	
+	if(qtde > 0){
+		DeclVar(BUFFER, escopo);
+		token = head(BUFFER);
+	}else{
+		if(strcmp(lookahead(BUFFER), "var") == 0){
+			while(strcmp(lookahead(BUFFER), "var") == 0){
+				match(BUFFER, "var");
+				DeclVar(BUFFER, escopo);
+				token = head(BUFFER);
+		    }
+	    }else{
+	        printf("Era esperado <var>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+			exit(1);
+		}
+	}
+		
+} 
+
+//NUMERO -> DIGITO {DIGITO} 
+void NUMERO(node *BUFFER, int escopo){ 
+    node *token;
+	token = head(BUFFER);
+	
+	if(strcmp(lookahead(BUFFER), "numero") == 0){
+		match(BUFFER, "numero");
+	}else{
+		printf("Era esperado <numero>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+		exit(1);
+	}
+
+}
+
+//logico -> verdadeiro | falso
+void BOOL(node *BUFFER,  int escopo){
+	node *token;
+	token = head(BUFFER);
+	if(strcmp(lookahead(BUFFER), "verdadeiro") == 0){
+		match(BUFFER, "verdadeiro");
+	}else if(strcmp(lookahead(BUFFER), "falso") == 0){
+		match(BUFFER, "falso");
+	}else{
+		 printf("Era esperado <verdadeiro> ou <falso>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+		exit(1);
+	}
+}
+
+//Atribuição de variável
+void ATRIB(node *BUFFER, int escopo){
+	node *token;
+	ID(BUFFER, escopo);
+	token = head(BUFFER);
+	if(strcmp(lookahead(BUFFER), "opatrib") == 0){
+		match(BUFFER, "opatrib");
+		Expr(BUFFER, escopo);
+	}else{
+		printf("Era esperado <opatrib>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+		exit(1);
+	}
+}
+//Comandos -> 
+void CMD(node *BUFFER, int escopo){
+	node *token;
+	
+	if(strcmp(lookahead(BUFFER), "escreva") == 0){
+		
+			match(BUFFER, "escreva");
+			token = head(BUFFER);
+        	if(strcmp(lookahead(BUFFER), "oppa") == 0){
+        		match(BUFFER, "oppa");
+				ID(BUFFER, escopo);
+			}else{
+				printf("Era esperado <oppa>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+				exit(1);
+			}
+			token = head(BUFFER);
+        	if(strcmp(lookahead(BUFFER), "oppf") == 0){
+        		match(BUFFER, "oppf");
+			}else{
+				printf("Era esperado <)>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+				exit(1);
+			}
+	}else if(strcmp(lookahead(BUFFER), "leia") == 0){
+		
+			match(BUFFER, "leia");
+			token = head(BUFFER);
+        	if(strcmp(lookahead(BUFFER), "oppa") == 0){
+        		match(BUFFER, "oppa");
+				ID(BUFFER, escopo);
+			}else{
+				printf("Era esperado <oppa>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+				exit(1);
+			}
+			token = head(BUFFER);
+        	if(strcmp(lookahead(BUFFER), "oppf") == 0){
+        		match(BUFFER, "oppf");
+			}else{
+				printf("Era esperado <)>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+				exit(1);
+			}
+	}else if(strcmp(lookahead(BUFFER), "id") == 0){
+		ATRIB(BUFFER, escopo);
+	}else if(strcmp(lookahead(BUFFER), "se") == 0){
+		CONDICAOIF(BUFFER, escopo);
+	}else if(strcmp(lookahead(BUFFER), "para") == 0){
+		CONDICAOPARA(BUFFER, escopo);
+	}else if(strcmp(lookahead(BUFFER), "enquanto") == 0){
+		CONDICAOENQUANTO(BUFFER, escopo);
+	}
+}
+
+
+void CONDICAOELSE(node *BUFFER, int escopo){
+	node *token;
+	token = head(BUFFER);
+	if(strcmp(lookahead(BUFFER), "senao") == 0){
+		match(BUFFER, "senao");
+		CMD(BUFFER,  escopo);
+		
+		while(strcmp(lookahead(BUFFER), "fimpara") != 0){
+			if(strcmp(lookahead(BUFFER), "fimalgoritmo") == 0){
+				token = head(BUFFER);
+				printf("Era esperado <fimpara>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+				exit(1);
+			}else{
+				CMD(BUFFER, escopo);
+			}		
+		}
+		
+		if(strcmp(lookahead(BUFFER), "fimse") == 0){
+			match(BUFFER, "fimse");
+		}else{
+			printf("Era esperado <fimse>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+			exit(1);
+		}
+	}else{
+		printf("Era esperado <senao>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+		exit(1);
+	}	
+}
+
+void CONDICAOIF(node *BUFFER, int escopo){
+	node *token;
+	token = head(BUFFER);
+	if(strcmp(lookahead(BUFFER), "se") == 0){
+		match(BUFFER, "se");
+		Expr(BUFFER, escopo);
+	    token = head(BUFFER);
+		if(strcmp(lookahead(BUFFER), "entao") == 0){
+			match(BUFFER, "entao");
+		    CMD(BUFFER, escopo);
+			while(strcmp(lookahead(BUFFER), "fimse") != 0){
+				if(strcmp(lookahead(BUFFER), "fimalgoritmo") == 0 || strcmp(lookahead(BUFFER), "fimpara") == 0 || strcmp(lookahead(BUFFER), "fimenquanto") == 0){
+					token = head(BUFFER);
+					printf("Era esperado <fimse>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+					exit(1);
+				}else{
+					CMD(BUFFER, escopo);
+				}		
+			}
+			
+		    if(strcmp(lookahead(BUFFER), "fimse") == 0){
+				match(BUFFER, "fimse");
+			}else{
+				printf("Era esperado <fimse>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+				exit(1);
+			}
+	    }else{
+		    printf("Era esperado <entao>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+			exit(1);
+        }
+	    if(strcmp(lookahead(BUFFER), "senao") == 0){
+	    	CONDICAOELSE(BUFFER, escopo);
+		}
+	}else{
+		printf("Era esperado <se>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+		exit(1);
+	}	
+}
+
+void RELACAO(node *BUFFER,  int escopo){
+	node *token;
+	token = head(BUFFER);
+	if(strcmp(lookahead(BUFFER), "opigual") == 0){
+		match(BUFFER, "opigual");
+	}else if(strcmp(lookahead(BUFFER), "opmen") == 0){
+		match(BUFFER, "opmen");
+	}else if(strcmp(lookahead(BUFFER), "opmai") == 0){
+		match(BUFFER, "opmai");
+	}else if(strcmp(lookahead(BUFFER), "opmaiigual") == 0){
+		match(BUFFER, "opmaiigual");
+	}else if(strcmp(lookahead(BUFFER), "opmenigual") == 0){
+		match(BUFFER, "opmenigual");
+	}else if(strcmp(lookahead(BUFFER), "opdifer") == 0){
+		match(BUFFER, "opdifer");
+	}else{
+		printf("Era esperado operador relacional ,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+		exit(1);
+	}
+
+}
+
+void Expr(node *BUFFER, int escopo){
+	ExprSimples(BUFFER, escopo);
+	while(strcmp(lookahead(BUFFER), "opigual") == 0||strcmp(lookahead(BUFFER), "opmai") == 0||strcmp(lookahead(BUFFER), "opmen") == 0||strcmp(lookahead(BUFFER), "opmaiigual") == 0||strcmp(lookahead(BUFFER), "opmenigual") == 0||strcmp(lookahead(BUFFER), "opdifer") == 0){
+	    RELACAO(BUFFER,escopo);
+    	ExprSimples(BUFFER, escopo);
+    }
+}
+
+void FATOR(node *BUFFER, int escopo){
+	node *token;
+	token = head(BUFFER);
+	if(strcmp(lookahead(BUFFER), "id") == 0){
+		match(BUFFER, "id");
+	}else if(strcmp(lookahead(BUFFER), "numero") == 0){
+		match(BUFFER, "numero");
+	}else if(strcmp(lookahead(BUFFER), "verdadeiro") == 0){
+		match(BUFFER, "verdadeiro");
+	}else 	if(strcmp(lookahead(BUFFER), "falso") == 0){
+		match(BUFFER, "false");
+	}else{
+		printf("Era esperado <id>, <numero>, <verdadeiro ou <falso>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+		exit(1);
+	}
+}
+
+//ExprSimples -> Operações aritmericas, verificação de variaveis e atribuição
+void ExprSimples(node *BUFFER, node *TSIMBOLO, int escopo){
+	node *token;
+	token = head(BUFFER);
+	while(strcmp(lookahead(BUFFER), "falso") == 0||strcmp(lookahead(BUFFER), "verdadeiro") == 0||strcmp(lookahead(BUFFER), "id") == 0||strcmp(lookahead(BUFFER), "numero") == 0||strcmp(lookahead(BUFFER), "opsoma") == 0||strcmp(lookahead(BUFFER), "opsub") == 0 ||strcmp(lookahead(BUFFER), "opdiv") == 0 ||strcmp(lookahead(BUFFER), "opmult") == 0 || strcmp(lookahead(BUFFER), "opatrib") == 0 ){
+		
+		if(strcmp(lookahead(BUFFER), "opsoma") == 0){
+			match(BUFFER, "opsoma");
+			FATOR(BUFFER, escopo);
+		}else if(strcmp(lookahead(BUFFER), "falso") == 0){
+			FATOR(BUFFER, escopo);
+		}else if(strcmp(lookahead(BUFFER), "verdadeiro") == 0){
+			FATOR(BUFFER,  escopo);
+		}else if(strcmp(lookahead(BUFFER), "id") == 0){
+			FATOR(BUFFER, escopo);
+		}else if(strcmp(lookahead(BUFFER), "numero") == 0){
+			FATOR(BUFFER,  escopo);
+		}else if(strcmp(lookahead(BUFFER), "opsub") == 0){
+			match(BUFFER, "opsub");
+			FATOR(BUFFER,  escopo);
+		}else if(strcmp(lookahead(BUFFER), "opdiv") == 0){
+			match(BUFFER, "opdiv");
+			FATOR(BUFFER,  escopo);
+		}else if(strcmp(lookahead(BUFFER), "opmult") == 0){
+			match(BUFFER, "opmult");
+			FATOR(BUFFER ,escopo);
+		}else if(strcmp(lookahead(BUFFER), "opatrib") == 0){
+			match(BUFFER, "opatrib");
+			FATOR(BUFFER ,escopo);
+		}else{
+			 printf("Era esperado um operador aritmetico, variavel ou numero,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+			 exit(1);
+		}
+	}
+}
+
+
+//ENQUANTO
+void CONDICAOENQUANTO(node *BUFFER, int escopo){
+	node *token;
+	token = head(BUFFER);
+	if(strcmp(lookahead(BUFFER), "enquanto") == 0){
+		    match(BUFFER, "enquanto");
+		    Expr(BUFFER, escopo);   
+		    token = head(BUFFER);	
+		if(strcmp(lookahead(BUFFER), "faca") == 0){
+			match(BUFFER, "faca");
+		    CMD(BUFFER, escopo);
+		    
+			while(strcmp(lookahead(BUFFER), "fimenquanto") != 0){
+				if(strcmp(lookahead(BUFFER), "fimalgoritmo") == 0 || strcmp(lookahead(BUFFER), "fimpara") == 0 || strcmp(lookahead(BUFFER), "fimse") == 0){
+					token = head(BUFFER);
+					printf("Era esperado <fimenquanto>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+					exit(1);
+				}else{
+					CMD(BUFFER, escopo);
+				}		
+			}
+			
+		    if(strcmp(lookahead(BUFFER), "fimenquanto") == 0){
+				match(BUFFER, "fimenquanto");
+			}else{
+				printf("Era esperado <fimenquanto>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+				exit(1);
+			}
+		    
+	    }else{
+		    printf("Era esperado <faca>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+			exit(1);
+        }
+	}else{
+			printf("Era esperado <enquanto>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+			exit(1);
+	}	
+}
+
+//PARA
+void CONDICAOPARA(node *BUFFER, node *TSIMBOLO, int escopo){
+	node *token;
+	token = head(BUFFER);
+	if(strcmp(lookahead(BUFFER), "para") == 0){
+		    match(BUFFER, "para");
+		    ID(BUFFER, escopo);   
+		    token = head(BUFFER);	
+		if(strcmp(lookahead(BUFFER), "de") == 0){
+			match(BUFFER, "de");
+		    NUMERO(BUFFER, escopo);
+		    if(strcmp(lookahead(BUFFER), "ate") == 0){
+				match(BUFFER, "ate");
+				NUMERO(BUFFER, escopo);
+				if(strcmp(lookahead(BUFFER), "passo") == 0){
+					match(BUFFER, "passo");
+					NUMERO(BUFFER, escopo);
+					if(strcmp(lookahead(BUFFER), "faca") == 0){
+						match(BUFFER, "faca");
+					    CMD(BUFFER, escopo);
+					    while(strcmp(lookahead(BUFFER), "fimpara") != 0){
+					    	if(strcmp(lookahead(BUFFER), "fimalgoritmo") == 0 || strcmp(lookahead(BUFFER), "fimse") == 0 || strcmp(lookahead(BUFFER), "fimenquanto") == 0){
+					    		token = head(BUFFER);
+					    		printf("Era esperado <fimpara>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+								exit(1);
+							}else{
+								CMD(BUFFER, escopo);
+							}		
+						}
+					    
+					    if(strcmp(lookahead(BUFFER), "fimpara") == 0){
+							match(BUFFER, "fimpara");
+						}else{
+							printf("Era esperado <fimpara>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+							exit(1);
+						}
+					}else{
+						printf("Era esperado <faca>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+						exit(1);
+					}
+				}else{
+					printf("Era esperado <passo>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+					exit(1);
+				}
+			}else{
+				printf("Era esperado <ate>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+			exit(1);
+			}
+		    
+	    }else{
+		    printf("Era esperado <de>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+			exit(1);
+        }
+	}else{
+			printf("Era esperado <para>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+			exit(1);
+	}	
+}
+
+
+//Conjunto de comandos contemplando o inicio
+void CmdComposto(node *BUFFER, int escopo){	
+    node *token, *tabela;
+	token = head(BUFFER);
+	if (strcmp(lookahead(BUFFER), "inicio") == 0) {
+		escopo++;
+		match(BUFFER, "inicio");
+		CMD(BUFFER, escopo);   
+		 
+		while((strcmp(lookahead(BUFFER), "fimalgoritmo") != 0)){
+			CMD(BUFFER, escopo);
+	    }
+	}else{ 
+           printf("Era esperado <inicio>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+	       exit(1);
+    }    
+}
+
+//BLOCO -> [PartDeclVar] Seçao de comandos
+void BLOCO(node *BUFFER, int escopo){
+	int qtde = 0;
+	while(strcmp(lookahead(BUFFER), "inicio") != 0){
+	    PartDeclVar(BUFFER, escopo, qtde++); 
+    }
+
+	 CmdComposto(BUFFER, escopo);
+}
+
+void algoritmo(node *BUFFER,int escopo){	
+    node *token;
+	ID(BUFFER, escopo);	
+	token = head(BUFFER);
+	
+	BLOCO(BUFFER, escopo);
+	token = head(BUFFER);
+			
+	if (strcmp(lookahead(BUFFER), "fimalgoritmo") == 0) {
+		match(BUFFER, "fimalgoritmo");
+	}else{
+		printf("Era esperado <fimalgoritmo>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+	}
+	
+}
+
+void analisador_sintatico(node *BUFFER, int stop){
+	node *token;
+	token = head(BUFFER);
+	int escopo = 0;
+	char *retorno;
+	retorno = lookahead(BUFFER);
+	
+	while(stop != -1){
+		if(strcmp(retorno, "algoritmo") == 0){	
+				match(BUFFER, "algoritmo");
+				algoritmo(BUFFER, escopo);
+				stop = 1;
+			}else{
+			    printf("Era esperado <algoritmo>,porem foi encontrado <%s> na linha %d\n", token->token, token->linha);
+			    stop = -1;
+				break;	
+		}
+		printf("Analise sintatica finalizada. Codigo sem erros.\n");
+	}
+	printf("Analise sintatica finalizada. Codigo sem erros.\n"); 
+}
+
 
 int main(){
 	char str[80], linha[80], nomeToken[100];
@@ -1259,9 +1792,11 @@ int main(){
 	FILE *arq;
 	FILE *saida;
 	node *BUFFER=(node *)malloc(sizeof(node));
+	node *listaTokens=(node *)malloc(sizeof(node));
     inicia(BUFFER);
+    inicia(listaTokens);
     int posicao;
-    
+    int linhaArquivo = 0;
 	arq = fopen("teste.txt", "r");
 	
 	saida = fopen("saida.txt", "w");
@@ -1270,6 +1805,7 @@ int main(){
 		printf("Erro, nao foi possivel abrir o arquivo\n");
 	else{
 		while((fgets(linha, 500, arq)!= NULL)&&(nomeToken!=0)){	
+			linhaArquivo++;
 			if(!startsWith(linha, "//")) { 
 	    		 lexema = strtok( linha, " \n\t" );
 	    		 
@@ -1277,7 +1813,7 @@ int main(){
 					strcpy(nomeToken, leitorLexico(lexema, &i));
 				    if(strcmp (nomeToken, "erro") != 0){
 			    	    if(strcmp(nomeToken, "id") == 0){
-			    	    	enqueue(BUFFER,nomeToken,lexema);
+			    	    	enqueue(BUFFER,nomeToken,lexema, linhaArquivo);
 			    	    	posicao = buscaSimbolo(BUFFER, lexema);
 			    	    	sprintf(lexema, "%d", posicao);
 						}
@@ -1286,6 +1822,7 @@ int main(){
 						}else{
 							fprintf(saida, "<%s,%s>\n", nomeToken, lexema);	
 						}
+						enqueue(listaTokens,nomeToken, lexema, linhaArquivo );
 				    	quantidade++;
 				    	}else{
 				    		if(nomeToken=="erro"){
@@ -1299,8 +1836,10 @@ int main(){
 			}	
 			    
 		   }    
+		
 		fclose(saida);
 		fclose(arq);
+		analisador_sintatico(listaTokens, quantidade);
 		}
 		
 	libera(BUFFER);
